8824abccaf9c223fa6241bb6da093e1098c5c6e0
niciuoss <viniciussales.dev@gmail.com>
2024-10-22 11:20:21 -0300

    correção de arquivo

--- crud_api/src/test/java/com/example/crud_api/CrudApiApplicationTests.java
+++ crud_api/src/test/java/com/example/crud_api/CrudApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CrudApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
