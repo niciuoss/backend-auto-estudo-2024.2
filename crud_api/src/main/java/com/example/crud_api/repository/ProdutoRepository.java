5f6ee49defc086834ed4bba1fbb0d0f575ac2aed
Vinicius Sales <viniciusamorimsales@gmail.com>
2024-10-27 22:31:28 -0300

    novas alteracoes

--- crud_api/src/main/java/com/example/crud_api/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud_api.repository;
-
-import com.example.crud_api.models.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
